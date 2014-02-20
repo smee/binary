21e64a1aa06f5e3c4885f3eaba7cebe99f899900
zsau <qjhjdm@gmail.com>
2014-02-20 00:09:59 -0600

    [internal] fix whitespace

--- src/org/clojars/smee/binary/core.clj
+++ src/org/clojars/smee/binary/core.clj
@@ -10,7 +10,7 @@
 (defn codec? [codec]
   (satisfies? BinaryIO codec))
 
-(defmacro ^:private primitive-codec 
+(defmacro ^:private primitive-codec
   "Create an reification of `BinaryIO` that can read/write a primmitive data type."
   [get-fn write-fn cast-fn & [endianess]]
   (let [big-in     (gensym "big-in")
@@ -37,11 +37,11 @@
 (def primitive-codecs
   {:byte (primitive-codec .readByte .writeByte byte)
    :ubyte (primitive-codec .readUnsignedByte .writeByte byte->ubyte)
-   
+
    ;:char (primitive-codec .readChar .writeChar char :be)
    ;:char-le (primitive-codec .readChar .writeChar char :le)
    ;:char-be (primitive-codec .readChar .writeChar char :be)
-   
+
    :short    (primitive-codec .readShort .writeShort short :be)
    :short-le (primitive-codec .readShort .writeShort short :le)
    :short-be (primitive-codec .readShort .writeShort short :be)
@@ -59,7 +59,7 @@
    :float    (primitive-codec .readFloat .writeFloat float :be)
    :float-le (primitive-codec .readFloat .writeFloat float :le)
    :float-be (primitive-codec .readFloat .writeFloat float :be)
-   
+
    :double    (primitive-codec .readDouble .writeDouble double :be)
    :double-le (primitive-codec .readDouble .writeDouble double :le)
    :double-be (primitive-codec .readDouble .writeDouble double :be)
@@ -67,58 +67,58 @@
 
 (declare compile-codec)
 
-(defn ordered-map 
+(defn ordered-map
   "Parse a binary stream into a map."
   [& kvs]
-  {:pre [(even? (count kvs))]} 
+  {:pre [(even? (count kvs))]}
   (let [ks (take-nth 2 kvs)
         vs (take-nth 2 (rest kvs))
         key-order (into {} (map-indexed #(vector %2 %) ks))
         internal-map (apply sorted-map-by (comparator #(< (key-order % java.lang.Long/MAX_VALUE) (key-order %2 java.lang.Long/MAX_VALUE))) kvs)]
-    (reify 
+    (reify
       BinaryIO
       (read-data  [_ big-in little-in]
         (zipmap ks (map #(read-data % big-in little-in) vs)))
-      (write-data [_ big-out little-out value] 
+      (write-data [_ big-out little-out value]
         {:pre [(every? (set ks) (keys value))]}
-        (dorun (map #(write-data % big-out little-out %2) 
+        (dorun (map #(write-data % big-out little-out %2)
                     vs
                     (map #(get value %) ks))))
-      
+
       java.lang.Object
-       (toString [this] 
+       (toString [this]
          (str internal-map))
-      
+
       clojure.lang.ILookup
       (valAt [_ key]
         (get internal-map key))
       (valAt [_ key not-found]
         (get internal-map key not-found))
-      
+
       clojure.lang.Counted
       (count [_]
          (count internal-map))
-      
+
 ;       clojure.lang.Associative
-;       (containsKey [_ k] 
+;       (containsKey [_ k]
 ;         (contains? internal-map k))
 ;       (entryAt [_ k]
 ;         (get internal-map k))
-;       (assoc [this k v]  
+;       (assoc [this k v]
 ;         (apply ordered-map (apply concat (seq (assoc internal-map k v)))))
-       
+
        clojure.lang.IPersistentMap
        (assoc [this k v]
          (apply ordered-map (apply concat (seq (assoc internal-map k v)))))
        (assocEx [this k v]
-         (if (internal-map k) 
+         (if (internal-map k)
            (throw (ex-info "Key already present" {:key k}))
            (apply ordered-map (apply concat (seq (assoc internal-map k v))))))
        (without [this k]
          (apply ordered-map (apply concat (seq (dissoc internal-map k)))))
-       
+
        clojure.lang.IPersistentCollection
-       (cons [this [k v]]  
+       (cons [this [k v]]
          (assoc this k v))
        (empty [_]
          (ordered-map))
@@ -126,47 +126,47 @@
          false)
 
        clojure.lang.Seqable
-       (seq [_] 
+       (seq [_]
          (seq internal-map))
 
        ;; Java interfaces
        java.lang.Iterable
-       (iterator [this] 
+       (iterator [this]
          (.iterator (seq this))))))
 
-(defn- read-times 
+(defn- read-times
   "Performance optimization for `(repeatedly n #(read-data codec big-in little-in))`"
   [n codec big-in little-in]
-  (loop [n (int n), res (transient [])] 
-    (if (zero? n) 
-      (persistent! res) 
+  (loop [n (int n), res (transient [])]
+    (if (zero? n)
+      (persistent! res)
       (recur (dec n) (conj! res (read-data codec big-in little-in))))))
 
-(defn- read-exhausting 
+(defn- read-exhausting
   "Performance optimization for `(take-while (complement nil? )(repeatedly n #(read-data codec big-in little-in)))`"
   [codec big-in little-in]
-  (loop [res (transient [])] 
+  (loop [res (transient [])]
     (if-let [value (try (read-data codec big-in little-in) (catch java.io.EOFException e nil))]
       (recur (conj! res value))
       (persistent! res))))
 
-(defn- read-until-separator 
+(defn- read-until-separator
   "Read until the read value equals `separator`."
-  [codec big-in little-in separator] 
-  (loop [res (transient []), empty? true] 
-    (let [value (try 
-                  (read-data codec big-in little-in) 
-                  (catch java.io.EOFException e 
-                    (if empty? ;there is no value read yet, but the stream is empty 
+  [codec big-in little-in separator]
+  (loop [res (transient []), empty? true]
+    (let [value (try
+                  (read-data codec big-in little-in)
+                  (catch java.io.EOFException e
+                    (if empty? ;there is no value read yet, but the stream is empty
                       (throw e)
                       ;else: there seems to be no more bytes, so just return what we have
                       separator)))]
-      (if 
-        (= value separator) 
+      (if
+        (= value separator)
         (persistent! res)
         (recur (conj! res value) false)))))
 
-(defn repeated 
+(defn repeated
   "Read a sequence of values. Options are pairs of keys and values with possible keys:
 - `:length` fixed length of the sequence
 - `:prefix` codec for the length of the sequence to read prior to the sequence itself.
@@ -182,22 +182,22 @@ Example: To read a sequence of integers with a byte prefix for the length use `(
         length (options :length)
         prefix (options :prefix)
         separator (options :separator)]
-    (cond length (reify BinaryIO 
+    (cond length (reify BinaryIO
                    (read-data  [_ big-in little-in]
                      (read-times length codec big-in little-in))
                    (write-data [_ big-out little-out values]
-                     (if (not= length (count values)) 
+                     (if (not= length (count values))
                        (throw (java.lang.IllegalArgumentException. (str "This sequence should have length " length " but has really length " (count values))))
-                       (doseq [value values] 
+                       (doseq [value values]
                          (write-data codec big-out little-out value)))))
           ; use prefix-codec?
-          prefix (let [prefix-codec (compile-codec prefix)] 
-                   (reify BinaryIO 
+          prefix (let [prefix-codec (compile-codec prefix)]
+                   (reify BinaryIO
                      (read-data  [_ big-in little-in]
                        (let [length (read-data prefix-codec big-in little-in)]
                          (read-times length codec big-in little-in)))
                      (write-data [_ big-out little-out values]
-                       (let [length (count values)] 
+                       (let [length (count values)]
                          (write-data prefix-codec big-out little-out length)
                          (dorun (map #(write-data codec big-out little-out %) values))))))
           separator (reify BinaryIO
@@ -214,31 +214,31 @@ Example: To read a sequence of integers with a byte prefix for the length use `(
                     (doseq [value values]
                       (write-data codec big-out little-out value)))))))
 
-(defn constant 
+(defn constant
   "Reads a constant value, ignores given value on write. Can be used as a version tag for a composite codec.
 Example:
     (encode out (constant :int-le 7) 1234)
     => ;will instead write bytes [7 0 0 0]"
   [codec constant-value]
-  (compile-codec codec 
-                 (constantly constant-value) 
-                 #(do 
-                    (assert (= % constant-value) (format "value '%s' should have had the constant value '%s'" (str %) (str constant-value))) 
+  (compile-codec codec
+                 (constantly constant-value)
+                 #(do
+                    (assert (= % constant-value) (format "value '%s' should have had the constant value '%s'" (str %) (str constant-value)))
                     constant-value)))
 
 (defn string [^String encoding & options]
   {:pre [(some #{:length :prefix :separator} (take-nth 2 options))]}
-  (compile-codec 
+  (compile-codec
     (apply repeated :byte options)
     (fn string2bytes [^String s] (.getBytes s encoding))
     #(String. (byte-array %) encoding)))
 
 
 
-(defn c-string 
+(defn c-string
   "Zero-terminated string (like in C). String is a sequence of bytes, terminated by a 0 byte."
   [^String encoding]
-  (compile-codec 
+  (compile-codec
     (repeated :byte :separator (byte 0))
     (fn string2bytes [^String s] (.getBytes s encoding))
     #(String. (byte-array %) encoding)))
@@ -251,7 +251,7 @@ Example:
   (update-in bytes [(- (count bytes) 1 (quot idx 8))]
                    #(bit-or % (bit-shift-left 1 (mod idx 8)))))
 
-(defn bits 
+(defn bits
   "`flags` is a sequence of flag names. Each flag's index corresponds to the bit with that index.
 Flag names `null` are ignored. Bit count will be padded up to the next multiple of 8."
   [flags]
@@ -263,13 +263,13 @@ Flag names `null` are ignored. Bit count will be padded up to the next multiple 
                    (fn [flags] (reduce #(set-bit % %2) (into [] (byte-array byte-count)) (vals (select-keys flags->idx flags))))
                    (fn [bytes] (set (map idx->flags (filter #(bit-set? bytes %) bit-indices)))))))
 
-(defn header 
+(defn header
   "Decodes a header using `header-codec`. Passes this datastructure to `header->body` which returns the codec to
 use to parse the body. For writing this codec calls `body->header` with the data as parameter and
 expects a value to use for writing the header information."
   [header-codec header->body-codec body->header]
   (let [header-codec (compile-codec header-codec)]
-    (reify BinaryIO 
+    (reify BinaryIO
       (read-data  [_ big-in little-in]
         (let [header (read-data header-codec big-in little-in)
               body-codec (header->body-codec header)
@@ -277,11 +277,11 @@ expects a value to use for writing the header information."
           body))
       (write-data [_ big-out little-out value]
         (let [header (body->header value)
-              body-codec (header->body-codec header)] 
+              body-codec (header->body-codec header)]
           (write-data header-codec big-out little-out header)
           (write-data body-codec big-out little-out value))))))
 
-(defn padding 
+(defn padding
   "Make sure there is always a minimum byte `length` when reading/writing values.
 Works by reading `length` bytes into a byte array, then reading from that array using `inner-codec`.
 Currently there are two options:
@@ -295,16 +295,16 @@ Example:
     => ; writes bytes [97 98 99 0 100 101 102 0 103 104 105]
        ; observe: the last separator byte was truncated!"
   [inner-codec length & options]
-  {:pre [(number? length) (or (empty? options) 
+  {:pre [(number? length) (or (empty? options)
                               (number? (first options))
                               (even? (count options)))]}
   (let [inner-codec (compile-codec inner-codec)
-        options (cond 
+        options (cond
                   (even? (count options)) (apply hash-map options)
                   (number? (first options)) {:padding-byte (first options)}
                   :else (throw (ex-info "unknown options, please use onlye keys `:truncate?` and `:padding-byte`" {:wrong-options options})))
         {:keys [truncate? padding-byte] :or {padding-byte 0, truncate? false}} options]
-    (reify BinaryIO 
+    (reify BinaryIO
       (read-data  [_ big-in _]
         (let [bytes (byte-array length)
               _ (.readFully ^DataInput big-in bytes)
@@ -320,7 +320,7 @@ Example:
               arr (.toByteArray baos)
               len (if truncate? length (.size baos))
               padding-bytes-left (max 0 (- length len))]
-          (if (< (- length len) 0) 
+          (if (< (- length len) 0)
             (throw (ex-info (str "Data should be max. " length " bytes, but attempting to write " (Math/abs (- len length)) " bytes more!") {:overflow-bytes (Math/abs (- len length))}))
             (do
               (.write ^DataOutputStream big-out arr 0 len)
@@ -341,7 +341,7 @@ Only names and values in `m` will be accepted when encoding or decoding."
     (strict-map (map-invert m))))
 
 ;;;;;;; internal compilation of the DSL into instances of `BinaryIO`
-;; 
+;;
 ;; let sequences, vectors, maps and primitive's keywords implement BinaryIO
 ;; that means, compile-codec is optional!
 (extend-protocol BinaryIO
@@ -350,28 +350,28 @@ Only names and values in `m` will be accepted when encoding or decoding."
     (map #(read-data % big-in little-in) this))
   (write-data [this big-out little-out values]
     (dorun (map #(write-data % big-out little-out %2) this values)))
-  
+
   clojure.lang.IPersistentVector
   (read-data [this big-in little-in]
     (mapv #(read-data % big-in little-in) this))
   (write-data [this big-out little-out values]
     (dorun (map #(write-data % big-out little-out %2) this values)))
-  
+
   clojure.lang.Keyword
   (read-data [kw big-in little-in]
     (read-data (primitive-codecs kw) big-in little-in))
   (write-data [kw big-out little-out value]
     (write-data (primitive-codecs kw) big-out little-out value))
-  
+
   clojure.lang.IPersistentMap
   (read-data  [m big-in little-in]
     (zipmap (keys m) (map #(read-data % big-in little-in) (vals m))))
-  (write-data [m big-out little-out value] 
+  (write-data [m big-out little-out value]
     (dorun (map (fn [[k v]] (write-data (get m k) big-out little-out v)) value))))
 
-(defn compile-codec 
-  ([codec] (if (codec? codec) 
-             codec 
+(defn compile-codec
+  ([codec] (if (codec? codec)
+             codec
              (throw (ex-info (str codec " does not satisfy the protocol BinaryIO!" ) {:codec codec}))))
   ([codec pre-encode post-decode]
     (let [codec (compile-codec codec)]
@@ -383,14 +383,14 @@ Only names and values in `m` will be accepted when encoding or decoding."
 
 ;;;;;;;;;;;;;; API for en-/decoding
 
-(defn encode 
+(defn encode
   "Serialize a value to the outputstream out according to the codec."
   [codec out value]
   (let [big-out (BigEndianDataOutputStream. out)
         little-out (LittleEndianDataOutputStream. out)]
     (write-data codec big-out little-out value)))
 
-(defn decode 
+(defn decode
   "Serialize a value to the outputstream out according to the codec."
   [codec in]
   (let [big-in (BigEndianDataInputStream. in)
